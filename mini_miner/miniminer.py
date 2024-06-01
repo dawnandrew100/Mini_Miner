b90a23b17171e8c09a1089d54856fac80257cf31
Andrew Hennis <dawnandrew100@gmail.com>
2024-06-01 06:49:35 -0400

    initial project directory
    This is my initial layout for a coding challenge that I will be solving called the mini miner challenge from hackattic

---
+++ mini_miner/miniminer.py
@@ -0,0 +1,2 @@
+import requests
+from constants import token, challenge_url
---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# external
+from setuptools import setup
+
+setup(
+    name='Mini_Miner',
+    version='0.0.1',
+
+    packages=['Mini_Miner'],
+    package_data={'': ['*.json']},
+    python_requires='>=3.8',
+
+    url='https://github.com/dawnandrew100/Mini_Miner'
+)
