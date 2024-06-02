15bb274c4f1560c5145e301e50543e2b77ec65f2
Andrew Hennis <dawnandrew100@gmail.com>
2024-06-02 08:33:36 -0400

    Added GET request and edited .gitignore

--- mini_miner/constants.py
+++ mini_miner/constants.py
@@ -1,2 +1,10 @@
-challenge_url = 'https://hackattic.com/challenges/mini_miner/'
-token=''
+base_url = 'https://hackattic.com/challenges/mini_miner/problem?access_token='
+with open("token.txt", "r") as file:
+    #reads token in and removes whitespace + newline characters
+    token = file.readline().strip()
+
+challenge_url = base_url+token
+
+if __name__ == "__main__":
+    print(token)
+    print(challenge_url)
--- mini_miner/miniminer.py
+++ mini_miner/miniminer.py
@@ -1,2 +1,30 @@
-import requests
-from constants import token, challenge_url
+import requests as req
+from constants import challenge_url
+
+def main():
+    miniminer = Miniminer(challenge_url)
+    try:
+        miniminer.run()
+    except req.RequestException as e:
+        print(e)
+
+class _MiniminerINIT():
+    def __init__(self, challengeUrl):
+        self.challengeUrl = challengeUrl
+
+class MiniminerAPI(_MiniminerINIT):
+    def get(self):
+        response = req.get(self.challengeUrl)
+        return response
+
+class Miniminer(_MiniminerINIT):
+    def run(self):
+        api = MiniminerAPI(self.challengeUrl)
+        print("sending GET request...")
+        response = api.get()
+        if response.status_code != 200:
+            raise req.RequestException(f"BAD REQUEST! Status Code: {response.status_code}")
+        print(f"Received successful response! Status Code: {response.status_code}")
+
+if __name__ == "__main__":
+    main()
